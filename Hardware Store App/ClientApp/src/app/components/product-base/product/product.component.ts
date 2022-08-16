2cea4eee11b75087741e71d68edf9213080602dc
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-08-16 14:33:47 +0300

    A product component has been implemented.

--- Hardware Store App/ClientApp/src/app/components/product-base/product/product.component.ts
+++ Hardware Store App/ClientApp/src/app/components/product-base/product/product.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, Product {
 
-  constructor() { }
+  private service: ProductService;
 
-  ngOnInit(): void {
+  @Input() id: number = 0;
+  @Input() name: string = "";
+  @Input() description: string = "";
+  @Input() price: number = 0;
+  @Input() rating: number = 0;
+  @Input() categoryId: number = 0;
+  @Input() manufacturerId: number = 0;
+  @Input() countryProducerId: number = 0;
+
+  roundedRating: number = 0;
+
+  constructor(service: ProductService) {
+    this.service = service;
   }
-
+  
+  ngOnInit(): void {
+    this.roundedRating = Math.round(this.rating);
+  }
 }
