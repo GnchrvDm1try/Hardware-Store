1448c8e883a23fa6a3bc9c8b092fbc22c8f072a0
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-08-16 14:27:34 +0300

    A service that gets product data from API has been created.

---
+++ Hardware Store App/ClientApp/src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private APIUrl: string = environment.baseAPIUrl + "/api/product";
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Product[]>(this.APIUrl);
+  }
+
+  getProduct(id: number) {
+    return this.http.get<Product[]>(this.APIUrl + `/${id}`);
+  }
+}
