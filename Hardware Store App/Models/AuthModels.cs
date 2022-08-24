89013779013260ae1e9e3bb12bf6b91450dfeefa
Goncharov Dmitry <prototype1357@ukr.net>
2022-08-24 15:32:48 +0300

    Created authentication models.

---
+++ Hardware Store App/Models/AuthModels.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hardware_Store_App.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [MinLength(5)]
+        public string Email { get; set; } = String.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = String.Empty;
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        public string Firstname { get; set; } = String.Empty;
+        [Required]
+        public string Lastname { get; set; } = String.Empty;
+        [Required]
+        public string Password { get; set; } = String.Empty;
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; } = String.Empty;
+        [Required]
+        public string Email { get; set; } = String.Empty;
+        [MinLength(7)]
+        [MaxLength(16)]
+        public string PhoneNumber { get; set; } = String.Empty;
+        [Required]
+        public string Sex { get; set; } = String.Empty;
+        [Required]
+        public DateOnly Birthdate { get; set; }
+        public string Address { get; set; } = String.Empty;
+        public int Roleid { get; set; } = 2;
+    }
+}
