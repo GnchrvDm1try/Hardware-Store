70db9180ce201f1b8a00ee9848a3ec464ce8f725
Goncharov Dmitry <prototype1357@ukr.net>
2022-09-07 11:24:24 +0300

    Auth service has been created and partially implemented.

---
+++ Hardware Store App/ClientApp/src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private http: HttpClient;
+  private readonly APIUrl: string = environment.baseAPIUrl + "/api/account";
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  register(form: FormGroup): void {
+    this.http.post(this.APIUrl + "/register", form.getRawValue()).subscribe(
+      response => console.log(response),
+      error => console.error(error)
+    );
+  }
+}
