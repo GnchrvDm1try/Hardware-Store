30814e05934809db67aca3094119cd767e30dd12
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-09-09 12:38:30 +0300

    The login component has been created and implemented.

---
+++ Hardware Store App/ClientApp/src/app/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../../styles/forms.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  private readonly service: AuthService;
+  private readonly formBuilder: FormBuilder;
+  private readonly router: Router;
+
+  private JWTToken: object = {
+    "jwtToken": ""
+  };
+
+  constructor(formBuilder: FormBuilder, service: AuthService, router: Router) {
+    this.formBuilder = formBuilder;
+    this.service = service;
+    this.router = router;
+    this.form = this.getFormGroupInstance();
+  }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    this.service.login(this.form).subscribe(response => {
+      this.JWTToken = response
+    });
+  }
+
+  private getFormGroupInstance() {
+    let loginForm: FormGroup;
+    loginForm = this.formBuilder.group({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$")]),
+    });
+    return loginForm;
+  }
+}
