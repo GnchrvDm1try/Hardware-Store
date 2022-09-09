eb3ad67ced9ad8eb6bc8ca27e3f32e0434c44734
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-09-09 12:57:34 +0300

    A custom validator for passwords has been created.

--- Hardware Store App/ClientApp/src/app/components/register/register.component.ts
+++ Hardware Store App/ClientApp/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -10,17 +11,31 @@ import { AuthService } from '../../services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+
+  private readonly service: AuthService;
+  private readonly formBuilder: FormBuilder;
+  private readonly router: Router;
+
   private currentDateTime: Date = new Date();
   maxAllowedBirthDate: string = this.currentDateTime.getFullYear() - 6 + "-12" + "-31";
   minAllowedBirthDate: string = this.currentDateTime.getFullYear() - 100 + "-01" + "-01";
 
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(formBuilder: FormBuilder, service: AuthService, router: Router) {
     this.formBuilder = formBuilder;
+    this.service = service;
+    this.router = router;
     this.form = this.getFormGroupInstance();
   }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    this.service.register(this.form).subscribe(response => {
+      console.log(response);
+    });
+    this.router.navigateByUrl("/");
   }
 
   private getFormGroupInstance() {
@@ -35,7 +50,18 @@ export class RegisterComponent implements OnInit {
       address: new FormControl(null, Validators.minLength(7)),
       password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$")]),
       passwordConfirm: new FormControl(null, [Validators.required])
-    });
+    }, { validators: this.matchValidator("password", "passwordConfirm") });
     return registrationForm;
+  }
+
+  private matchValidator(source: string, target: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const sourceCtrl = control.get(source);
+      const targetCtrl = control.get(target);
+
+      return sourceCtrl && targetCtrl && sourceCtrl.value !== targetCtrl.value
+        ? { mismatch: true }
+        : null;
+    };
   }
 }
