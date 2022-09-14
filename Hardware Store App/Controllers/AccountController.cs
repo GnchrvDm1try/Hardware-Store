1ff50cbf54444fc3d51f3df14fc321bcb46148c5
Goncharov Dmitry <prototype1357@ukr.net>
2022-09-14 21:47:50 +0300

    Changed error handling in API register method.

--- Hardware Store App/Controllers/AccountController.cs
+++ Hardware Store App/Controllers/AccountController.cs
@@ -57,9 +57,15 @@ namespace Hardware_Store_App.Controllers
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            var user = await context.Users.Where(u => u.Email == model.Email || u.Phonenumber == model.PhoneNumber).FirstOrDefaultAsync();
+            var user = await context.Users.Where(u => u.Email == model.Email).FirstOrDefaultAsync();
 
-            if (user is not null && user.Phonenumber is not null) return BadRequest("User with such email or phone is already exists");
+            if (user is not null) return BadRequest("User with such email is already exists");
+
+            if (model.PhoneNumber is not null)
+            {
+                user = await context.Users.Where(u => u.Phonenumber == model.PhoneNumber).FirstOrDefaultAsync();
+                if (user is not null) return BadRequest("User with such phone number is already exists");
+            }
 
             await context.Users.AddAsync(new User(model));
             await context.SaveChangesAsync();
