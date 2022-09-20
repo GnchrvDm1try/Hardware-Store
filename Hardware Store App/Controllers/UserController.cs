ccebf1307bef4fee2429317b5c8f7eede39c3142
Fr1senn <bombavlad1@gmail.com>
2022-09-20 11:37:45 +0300

    A user controller has been created.

---
+++ Hardware Store App/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hardware_Store_App.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
