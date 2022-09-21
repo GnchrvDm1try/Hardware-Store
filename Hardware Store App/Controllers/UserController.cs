b43048c52f004a5bdfaec7171769d43e0c8211f9
Fr1senn <bombavlad1@gmail.com>
2022-09-21 11:20:15 +0300

    Implemented method which returns logged in user.

--- Hardware Store App/Controllers/UserController.cs
+++ Hardware Store App/Controllers/UserController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Hardware_Store_App.Models;
 
 namespace Hardware_Store_App.Controllers
 {
@@ -10,6 +13,22 @@ namespace Hardware_Store_App.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly StoreContext context;
+
+        public UserController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            int id = Convert.ToInt32(HttpContext.User.FindFirst("id")!.Value);
+            User? user = await this.context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user is null) return BadRequest("Couldn't find the user");
+            return Ok(user);
+        }
+
     }
 }
