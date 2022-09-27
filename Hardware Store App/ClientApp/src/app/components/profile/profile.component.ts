b07a3a4d52153f790c2b9ea24fe24edc6d9c606a
Fr1senn <bombavlad1@gmail.com>
2022-09-27 11:36:46 +0300

    Profile component gets the user from the service now.

--- Hardware Store App/ClientApp/src/app/components/profile/profile.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private readonly userService: UserService;
+  user: any;
 
-  constructor() { }
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(response => {
+      this.user = response
+      console.log(response)
+    });
   }
 
 }
