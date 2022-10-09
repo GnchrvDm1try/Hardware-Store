f5ea029061236bbef03b0ba282c75f1aa26915db
Fr1senn <bombavlad1@gmail.com>
2022-10-09 10:46:06 +0300

    Children routes of the ProfileBase module have been specified.

--- Hardware Store App/ClientApp/src/app/components/profile-base/profile-base.module.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/profile-base.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { OrderListComponent } from './order-list/order-list.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { EditComponent } from './edit/edit.component';
+import { RouterModule } from '@angular/router';
 import { CredentialsComponent } from './credentials/credentials.component';
+import { EditComponent } from './edit/edit.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,19 @@ import { CredentialsComponent } from './credentials/credentials.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'Profile', component: CredentialsComponent,
+        children: [
+          { path: 'Edit', component: EditComponent },
+          { path: 'Orders', component: OrderListComponent }
+        ]
+      },
+    ])
   ],
   exports: [
+    RouterModule,
     CredentialsComponent,
     EditComponent,
     OrderListComponent
