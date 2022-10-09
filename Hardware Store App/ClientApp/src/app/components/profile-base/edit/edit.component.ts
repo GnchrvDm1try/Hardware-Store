8a37c46b69138594f2adb985743e94668cba2249
Fr1senn <bombavlad1@gmail.com>
2022-10-09 18:36:36 +0300

    Created the form's field and added its initialization.

--- Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
@@ -9,6 +9,7 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  form!: FormGroup;
   user: any;
 
   private readonly userService: UserService;
@@ -25,10 +26,37 @@ export class EditComponent implements OnInit {
     this.router = router;
     this.userService.currentUser.subscribe(user => {
       this.user = user
+      this.form = this.getFormGroupInstance();
     });
   }
 
   ngOnInit(): void {
   }
 
+  private getFormGroupInstance() {
+    let registrationForm: FormGroup;
+    registrationForm = this.formBuilder.group({
+      firstName: new FormControl(this.user.firstname, [Validators.required, Validators.minLength(2), Validators.pattern("^[a-zA-Z]([a-zA-Z]| |-|')*$")]),
+      lastName: new FormControl(this.user.lastname, [Validators.required, Validators.minLength(2), Validators.pattern("^[a-zA-Z]([a-zA-Z]| |-|')*$")]),
+      email: new FormControl(this.user.email, [Validators.required, Validators.email]),
+      phoneNumber: new FormControl(this.user.phonenumber, [Validators.minLength(7), Validators.maxLength(16), Validators.pattern("^\\+\\d{6,15}$")]),
+      birthDate: new FormControl(`${this.user.birthdate.year}-${this.user.birthdate.month}-${this.user.birthdate.day}`, Validators.required),
+      sex: new FormControl(this.user.sex, [Validators.required, Validators.pattern("male|female")]),
+      address: new FormControl(this.user.address, Validators.minLength(7)),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$")]),
+      passwordConfirm: new FormControl(null, [Validators.required])
+    }, { validators: this.matchValidator("password", "passwordConfirm") });
+    return registrationForm;
+  }
+
+  private matchValidator(source: string, target: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const sourceCtrl = control.get(source);
+      const targetCtrl = control.get(target);
+
+      return sourceCtrl && targetCtrl && sourceCtrl.value !== targetCtrl.value
+        ? { mismatch: true }
+        : null;
+    };
+  }
 }
