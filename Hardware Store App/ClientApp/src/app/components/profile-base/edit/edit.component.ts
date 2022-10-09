a2e3a2f469b60fd99d0f43dc372ef6091be90c92
Fr1senn <bombavlad1@gmail.com>
2022-10-09 18:19:03 +0300

    Included required services.

--- Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  private readonly userService: UserService;
+  private readonly formBuilder: FormBuilder;
+  private readonly router: Router;
 
-  constructor() { }
+  constructor(userService: UserService, formBuilder: FormBuilder, router: Router) {
+    this.userService = userService;
+    this.formBuilder = formBuilder;
+    this.router = router;
+  }
 
   ngOnInit(): void {
   }
