130ca5159559e59222fab88b953a79305f611d89
Goncharov Dmitry <prototype1357@ukr.net>
2022-10-14 11:26:09 +0300

    The password field is no longer required.

--- Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/edit/edit.component.ts
@@ -58,8 +58,8 @@ export class EditComponent implements OnInit {
       birthDate: new FormControl(`${this.user.birthdate.year}-${this.user.birthdate.month}-${this.user.birthdate.day}`, Validators.required),
       sex: new FormControl(this.user.sex, [Validators.required, Validators.pattern("male|female")]),
       address: new FormControl(this.user.address, Validators.minLength(7)),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$")]),
-      passwordConfirm: new FormControl(null, [Validators.required])
+      password: new FormControl(null, [Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$")]),
+      passwordConfirm: new FormControl(null)
     }, { validators: this.matchValidator("password", "passwordConfirm") });
     return registrationForm;
   }
