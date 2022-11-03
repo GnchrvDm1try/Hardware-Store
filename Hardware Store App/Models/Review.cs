df5471657af29e65d8fab7e2ae238179d2c480a4
Fr1senn <bombavlad1@gmail.com>
2022-11-03 12:40:07 +0200

    Validation attributes have been added.

--- Hardware Store App/Models/Review.cs
+++ Hardware Store App/Models/Review.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hardware_Store_App.Models
 {
     public partial class Review
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
         public string Comment { get; set; } = null!;
         public DateTime Reviewdate { get; set; }
+        [Required]
+        [Range(1, 5)]
         public decimal Estimation { get; set; }
+        [Required]
         public int? Userid { get; set; }
+        [Required]
         public int? Productid { get; set; }
+
 
         public virtual Product? Product { get; set; }
         public virtual User? User { get; set; }
