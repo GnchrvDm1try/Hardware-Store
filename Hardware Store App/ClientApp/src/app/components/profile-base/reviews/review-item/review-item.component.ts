00f3b76990f0a2b727c86ec55d820a9d0d31c24c
Fr1senn <bombavlad1@gmail.com>
2022-11-03 10:53:46 +0200

    Implemented get property which outputs date correctly.

--- Hardware Store App/ClientApp/src/app/components/profile-base/reviews/review-item/review-item.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/reviews/review-item/review-item.component.ts
@@ -7,10 +7,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ReviewItemComponent implements OnInit {
   @Input() review: any;
+  private reviewDateTime: Date | undefined;
+
+  get dateTime() {
+    return `${this.reviewDateTime?.getHours().toString().padStart(2, '0')}:${this.reviewDateTime?.getMinutes().toString().padStart(2, '0')}
+      ${this.reviewDateTime?.getDate()?.toString()?.padStart(2, '0')}.${this.reviewDateTime?.getMonth()?.toString()?.padStart(2, '0')}.${this.reviewDateTime?.getFullYear()}`;
+  }
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.reviewDateTime = new Date(this.review.reviewdate)
   }
 }
