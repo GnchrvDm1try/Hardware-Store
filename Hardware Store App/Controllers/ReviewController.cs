d08b02ff9441670fe10732ea463afcc7ff2b38d6
Fr1senn <bombavlad1@gmail.com>
2022-11-03 12:40:54 +0200

    Implemented the edit method.

--- Hardware Store App/Controllers/ReviewController.cs
+++ Hardware Store App/Controllers/ReviewController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Hardware_Store_App.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +20,26 @@ namespace Hardware_Store_App.Controllers
         {
             this.context = context;
         }
-        
+
+        [HttpPatch("edit")]
+        public async Task<IActionResult> Edit([FromBody] Review review)
+        {
+            if (review is null) return BadRequest("Review was null");
+
+            Review? reviewFromDB = await context.Reviews.AsNoTracking().FirstOrDefaultAsync(r => r.Id == review.Id);
+            if (reviewFromDB is null) return BadRequest("Couldn't find review");
+
+            int userId = Convert.ToInt32(HttpContext.User.FindFirst("id")!.Value);
+            if (review.Userid != userId) return Unauthorized("Inappropriate user");
+
+            review.Reviewdate = reviewFromDB.Reviewdate;
+
+            context.Reviews.Update(review);
+            await context.SaveChangesAsync();
+
+            return Ok("Review has been updated");
+        }
+
         [HttpDelete("delete")]
         public async Task<IActionResult> Delete([FromBody] int id)
         {
