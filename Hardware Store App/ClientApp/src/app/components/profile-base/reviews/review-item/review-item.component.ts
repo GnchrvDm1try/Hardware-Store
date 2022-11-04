7f0b86059d9bfd7cd18523a51d80324e78171b5f
Fr1senn <bombavlad1@gmail.com>
2022-11-04 13:40:00 +0200

    Added field and method which gets edit form value.

--- Hardware Store App/ClientApp/src/app/components/profile-base/reviews/review-item/review-item.component.ts
+++ Hardware Store App/ClientApp/src/app/components/profile-base/reviews/review-item/review-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
@@ -10,7 +11,9 @@ export class ReviewItemComponent implements OnInit {
   @Input() review: any;
   @Output() removeEvent: any = new EventEmitter<any>();
   private readonly reviewService: ReviewService;
+  private readonly formBuilder: FormBuilder;
   private reviewDateTime: Date | undefined;
+  form!: FormGroup;
   isEditing: boolean = false;
   estimation: number = 1;
 
@@ -19,12 +22,14 @@ export class ReviewItemComponent implements OnInit {
       ${this.reviewDateTime?.getDate()?.toString()?.padStart(2, '0')}.${this.reviewDateTime?.getMonth()?.toString()?.padStart(2, '0')}.${this.reviewDateTime?.getFullYear()}`;
   }
 
-  constructor(reviewService: ReviewService) {
+  constructor(reviewService: ReviewService, formBuilder: FormBuilder) {
     this.reviewService = reviewService;
+    this.formBuilder = formBuilder;
   }
 
   ngOnInit(): void {
     this.reviewDateTime = new Date(this.review.reviewdate);
+    this.form = this.getFormGroupInstance();
     this.estimation = this.review.estimation;
   }
 
@@ -34,5 +39,17 @@ export class ReviewItemComponent implements OnInit {
 
   changeEstimation(index: number) {
     this.estimation = index + 1;
+  }
+
+  private getFormGroupInstance() {
+    let editForm: FormGroup;
+    editForm = this.formBuilder.group({
+      comment: new FormControl(this.review.comment, [Validators.required, Validators.maxLength(5000)]),
+      estimation: new FormControl(Math.round(this.review.estimation), [Validators.required]),
+      userId: new FormControl(this.review.userid),
+      productId: new FormControl(this.review.productid),
+      id: new FormControl(this.review.id)
+    });
+    return editForm;
   }
 }
