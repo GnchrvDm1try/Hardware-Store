a17d8ac4831a34005ef66190d35346b7b9ae3af7
Goncharov Dmitry <prototype1357@ukr.net>
2022-11-06 11:41:04 +0200

    Created an enum that consists of possible order statuses.

---
+++ Hardware Store App/ClientApp/src/app/models/enums/order-statuses.ts
@@ -0,0 +1,8 @@
+export enum OrderStatuses {
+  WaitingForApproving = 'Waiting for approving',
+  Delivering = 'Delivering',
+  WaitingForCustomer = 'Waiting for customer',
+  Completed = 'Completed',
+  Canceled = 'Canceled',
+  Returned = 'Returned'
+}
