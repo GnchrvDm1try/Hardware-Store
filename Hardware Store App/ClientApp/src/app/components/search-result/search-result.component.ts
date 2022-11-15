4e2b4f4368d1124ec12d4c3e19d1552201c86208
Fr1senn <bombavlad1@gmail.com>
2022-11-15 17:48:11 +0200

    Implemented a method which gets searched products.

--- Hardware Store App/ClientApp/src/app/components/search-result/search-result.component.ts
+++ Hardware Store App/ClientApp/src/app/components/search-result/search-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { SearchService } from "../../services/search.service";
 
 @Component({
   selector: 'app-search-result',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent implements OnInit {
+  private readonly route: ActivatedRoute;
+  private readonly searchService: SearchService;
+  products: any;
+  value: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(route: ActivatedRoute, searchService: SearchService) {
+    this.route = route;
+    this.searchService = searchService;
+    this.route.queryParams.subscribe(params => this.value = params.value);
   }
 
+  ngOnInit(): void {
+    this.search();
+  }
+
+  search() {
+    this.searchService.search(this.value).subscribe(products => this.products = products);
+  }
 }
