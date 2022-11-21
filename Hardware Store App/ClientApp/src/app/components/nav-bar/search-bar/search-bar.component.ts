96162c04452d804b58f6b50e64bf707eaa581afe
Fr1senn <bombavlad1@gmail.com>
2022-11-21 10:43:33 +0200

    Implemented a method that previews searched products.

--- Hardware Store App/ClientApp/src/app/components/nav-bar/search-bar/search-bar.component.ts
+++ Hardware Store App/ClientApp/src/app/components/nav-bar/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { SearchService } from "../../../services/search.service";
 
 @Component({
   selector: 'app-search-bar',
@@ -9,7 +10,9 @@ import { Router } from "@angular/router";
 export class SearchBarComponent implements OnInit {
   private static readonly MIN_SEARCH_LENGTH: number = 2;
   private readonly router: Router;
+  private readonly searchService: SearchService;
   searchValue: string = '';
+  resultProducts: any = [];
 
   get MIN_SEARCH_LENGTH() {
     return SearchBarComponent.MIN_SEARCH_LENGTH;
@@ -17,6 +20,7 @@ export class SearchBarComponent implements OnInit {
 
   constructor(router: Router, searchService: SearchService) {
     this.router = router;
+    this.searchService = searchService;
   }
 
   ngOnInit(): void {
@@ -27,5 +31,14 @@ export class SearchBarComponent implements OnInit {
       ['Search/'],
       {queryParams: {'value': this.searchValue}}
     );
+  }
+
+  previewSearch(searchValue: string) {
+    setTimeout(() => {
+      if (searchValue.length >= this.MIN_SEARCH_LENGTH && searchValue === this.searchValue)
+        this.searchService.search(this.searchValue).subscribe((products: any) => this.resultProducts = products)
+      else
+        this.resultProducts = [];
+    }, 750);
   }
 }
